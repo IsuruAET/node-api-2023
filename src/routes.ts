cf1e4af74ee36c919c417fc7e8c25f411fcd89a4
Isuru Godakanda <isuru.g@incentivio.com>
2023-04-18 01:31:24 +0530

    Initial commit

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,5 @@
 import { Express } from "express";
 import {
-  createProductHandler,
-  deleteProductHandler,
-  getProductHandler,
-  updateProductHandler,
-} from "controllers/product.controller";
-import {
   createUserSessionHandler,
   deleteSessionHandler,
   getUserSessionsHandler,
@@ -15,12 +9,6 @@ import {
   getCurrentUserHandler,
 } from "controllers/user.controller";
 import validateResource from "middleware/validateResource";
-import {
-  createProductSchema,
-  deleteProductSchema,
-  getProductSchema,
-  updateProductSchema,
-} from "schemas/product.schema";
 import { createSessionSchema } from "schemas/session.schema";
 import { createUserSchema } from "schemas/user.schema";
 import deserializeUser from "middleware/deserializeUser";
@@ -41,31 +29,6 @@ const routes = (app: Express) => {
   app.get("/api/sessions", deserializeUser, getUserSessionsHandler);
 
   app.delete("/api/sessions", deserializeUser, deleteSessionHandler);
-
-  // Product
-  app.post(
-    "/api/products",
-    [deserializeUser, validateResource(createProductSchema)],
-    createProductHandler
-  );
-
-  app.put(
-    "/api/products/:productId",
-    [deserializeUser, validateResource(updateProductSchema)],
-    updateProductHandler
-  );
-
-  app.get(
-    "/api/products/:productId",
-    validateResource(getProductSchema),
-    getProductHandler
-  );
-
-  app.delete(
-    "/api/products/:productId",
-    [deserializeUser, validateResource(deleteProductSchema)],
-    deleteProductHandler
-  );
 };
 
 export default routes;
